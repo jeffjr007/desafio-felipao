43c289a5c0c4d7233767f9812a5e14ff084febff
jeffjr007 <jeffjr007z@gmail.com>
2024-10-19 18:41:34 -0300

    Update DesafioCalculadoraPtRanks.js

--- DesafioCalculadoraPtRanks.js
+++ DesafioCalculadoraPtRanks.js
@@ -1,32 +1,31 @@
 //Desafio Calculadora de Partidas Rankeadas
 
 function calcularPartidasRanks(vitorias, derrotas) {
-    
     const saldo = vitorias - derrotas
     
     let nivel
     if (vitorias <= 10) {
-        nivel = "Ferro"
+        nivel = "Ferro" 
     } else if (vitorias >= 11 && vitorias <= 20) {
-        nivel = "Bronze"
+        nivel = "Bronze" 
     } else if (vitorias >= 21 && vitorias <= 50) {
-        nivel = "Prata"
+        nivel = "Prata" 
     } else if (vitorias >= 51 && vitorias <= 80) {
-        nivel = "Ouro"
+        nivel = "Ouro" 
     } else if (vitorias >= 81 && vitorias <= 90) {
-        nivel = "Diamante"
+        nivel = "Diamante" 
     } else if (vitorias >= 91 && vitorias <= 100) {
-        nivel = "Lendário"
+        nivel = "Lendário" 
     } else {
         nivel = "Imortal" 
     }
     
-    return { saldo, nivel };
+    return { saldo, nivel }
 }
 
+const vitorias = 10
+const derrotas = 10 
+const resultadoRank = calcularPartidasRanks(vitorias, derrotas)
 
-const vitorias = 101
-const derrotas = 10
-const resultadoRank = calcularPartidasRanks(vitorias, derrotas);
+console.log(`O Herói tem de saldo de ${resultadoRank.saldo} está no nível de ${resultadoRank.nivel}`)
 
-console.log(`O Herói tem de saldo de ${resultadoRank.saldo} está no nível de ${resultadoRank.nivel}`);
